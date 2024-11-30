1b4458306ecd9f0463c3505291e7d63544cb957e
mamun <morzmamun@gmail.com>
2024-11-30 21:38:04 +0600

    writing test unit test by vitest

---
+++ src/tests/intro.test.js
@@ -0,0 +1,11 @@
+import { describe, test, it, expect } from "vitest";
+
+describe("max", () => {
+    it("should return the first argument if it is greater than the second", () => {
+        // AAA -> testing pattern
+        // Arrange : 
+        // Act
+        // Assert
+        
+    })
+})
